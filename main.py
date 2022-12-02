ec2bdd3ebe8985894b9322511119cbb2e74941a6
susan <susan.belling@peachpayments.com>
2022-12-02 11:34:58 +0200

    create csv on states_to_learn save

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ while len(guessed_states) < 50:
     if answer_state == "Exit":
         missed_states = [state for state in states if state not in guessed_states]
         new_data = pandas.DataFrame(missed_states)
-        new_data.to_csv("states_to_learn")
+        new_data.to_csv("states_to_learn.csv")
         break
 
     if answer_state in states:
